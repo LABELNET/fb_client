f5a7f7e414a8d86f8e29c6c169b42b790f725bf3
LABLENET <1406046087@qq.com>
2017-03-02 14:41:13 +0800

    [init]  条件查讯

--- fb_server/fourbox/urls.py
+++ fb_server/fourbox/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from fourbox.views import FbDocList, FbDocDetail
+from fourbox.views import FbDocList, FbDocDetail, FbDocWeekList
 
 urlpatterns = [
     # doc
     url(r'^mdocs/$', FbDocList.as_view()),
     url(r'^mdocs/(?P<pk>[0-9]+)$', FbDocDetail.as_view()),
+    url(r'^mdocs/filter/$', FbDocWeekList.as_view()),
 ]
--- fb_server/fourbox/views.py
+++ fb_server/fourbox/views.py
@@ -1,33 +1,40 @@
 from rest_framework import generics
-from rest_framework import mixins
 
 from fourbox.models import MakeDoc
 from fourbox.serializer import FbDocSerializer
 
 
 # doc list
-class FbDocList(mixins.ListModelMixin,
-                mixins.CreateModelMixin,
-                generics.GenericAPIView):
+class FbDocList(generics.ListCreateAPIView):
     queryset = MakeDoc.objects.all()
     serializer_class = FbDocSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, args, kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, args, kwargs)
 
 
 # doc object
-class FbDocDetail(mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  generics.GenericAPIView):
+class FbDocDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = MakeDoc.objects.all()
     serializer_class = FbDocSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, args, kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.update(request, args, kwargs)
+# doc list week num
+class FbDocWeekList(generics.ListAPIView):
+    serializer_class = FbDocSerializer
+
+    def get_queryset(self):
+        week_num = self.request.query_params.get('week')
+        user_name = self.request.query_params.get('name')
+        date_year = self.request.query_params.get('year')
+
+        kwargs = {
+        }
+
+        if week_num is not None:
+            kwargs['week_num'] = week_num
+
+        if user_name is not None:
+            kwargs['user_name'] = user_name
+
+        if date_year is not None:
+            kwargs['date__year'] = date_year
+
+        return MakeDoc.objects.filter(**kwargs)
